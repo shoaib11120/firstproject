82f42f3e1c91d845aa3207d2598a9f57cd17a29b
shoaib11120 <mdshoaibkhan.1999@gmail.com>
2020-07-10 17:03:48 +0530

    Added slideshow

--- project/project/views.py
+++ project/project/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Post
 
 
 def home(request):
-	return render(request,'home.html',{'nav':'nav.html'})
+	posts=Post.published.order_by('-publish')[:3]
+	return render(request,
+		'home.html',
+		{'posts':posts,'nav':'nav.html'})
 
 def category(request):
 	return render(request,'category.html',{'nav':'nav.html'})
+
+def post_latest(request):
+	posts=Post.published.order_by('-pub_date')[:3]
+	return render(request,
+		'home.html',
+		{'posts':posts})
