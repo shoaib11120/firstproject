e1d7ca7035deb544d01b0af4fa64aed9229539f3
shoaib11120 <mdshoaibkhan.1999@gmail.com>
2020-08-20 12:49:16 +0530

    *Added some features

--- project/account/models.py
+++ project/account/models.py
@@ -1,13 +1,26 @@
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.conf import settings
 from uuid import uuid4
 import os
 
 
+#change image name
 def path_and_rename(instance,filename):
-    fn, ext = os.path.splitext(filename)
-    return "media/userPic/{id}{ext}".format(id=instance.pk, ext=ext)
+	upload_to='userpic/{}'.format(instance.user.username)
+	ext=filename.split('.')[-1]
+	print(instance.pk)
+	filename='{}.{}'.format("profilePic",ext)
+	return os.path.join(upload_to,filename)
+
+
+class OverwriteStorage(FileSystemStorage,):
+    def get_available_name(self, name, max_length=None):
+        # If the filename already exists, remove it as if it was a true file system
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name
 
 class Profile(models.Model):
         user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-        photo = models.ImageField(upload_to=path_and_rename,blank=True,null=True)
+        photo = models.ImageField(upload_to=path_and_rename,blank=True,null=True,storage=OverwriteStorage())
--- project/static/account/files/js/profile1.js
+++ project/static/account/files/js/profile1.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     $(".innerFormInput").css("border", "2px solid black");
     $(".innerFormInput").prop("disabled", false);
     $("#submitProfileButton").css("display","block")
+    $("#photoChooseButton").css("display","block")
     $(".innerPhoto").children().css("display","block")
     $(this).css("display","none")
   });
